a2d9eca62c57630b946ce8c0fbfa3d855c0de8d9
Nilson Morais <nilson.morais-filho@serpro.gov.br>
2018-05-29 15:18:46 -0300

    Removing unecessary requests Display only the Book Title

--- server.js
+++ server.js
@@ -6,17 +6,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var growl = require('notify-send');
 
-var loginDetails = {
-    email: process.env.PACKT_EMAIL,
-    password: process.env.PACKT_PASSWORD,
-    op: "Login",
-    form_id: "packt_user_login_form",
-    form_build_id: ""
-};
-
 var url = 'https://www.packtpub.com/packt/offers/free-learning';
-var loginError = 'Sorry, you entered an invalid email address and password combination.';
-var getBookUrl;
 var bookTitle;
 
 //we need cookies for that, therefore let's turn JAR on
@@ -28,59 +18,15 @@ console.log('----------- Packt Grab Started --------------');
 growl.normal.notify('grab-packt', 'Packt Grab Started');
 request(url, function(err, res, body) {
     if (err) {
-        growl.normal.notify('grab-packt', 'Request failed');
+        growl.normal.notify('grab-packt:', 'Request failed');
         console.error('Request failed');
         console.log('----------- Packt Grab Done --------------');
         return;
     }
-
+    
     var $ = cheerio.load(body);
-    getBookUrl = $("a.twelve-days-claim").attr("href");
     bookTitle = $(".dotd-title").text().trim();
-    var newFormId = $("input[type='hidden'][id^=form][value^=form]").val();
-
-    if (newFormId) {
-        loginDetails.form_build_id = newFormId;
-    }
-
-    request.post({
-        uri: url,
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded'
-        },
-        body: require('querystring').stringify(loginDetails)
-    }, function(err, res, body) {
-        if (err) {
-            console.error('Login failed');
-            growl.normal.notify('grab-packt', 'Login failed');
-            console.log('----------- Packt Grab Done --------------');
-            return;
-        };
-        var $ = cheerio.load(body);
-        var loginFailed = $("div.error:contains('"+loginError+"')");
-        if (loginFailed.length) {
-            growl.normal.notify('grab-packt', 'Login failed, please check your email address and password');
-            console.error('Login failed, please check your email address and password');
-            console.log('Login failed, please check your email address and password');
-            console.log('----------- Packt Grab Done --------------');
-            return;
-        }
-
-        request('https://www.packtpub.com' + getBookUrl, function(err, res, body) {
-            if (err) {
-                growl.normal.notify('grab-packt', 'Request error');
-                console.error('Request Error');
-                console.log('----------- Packt Grab Done --------------');
-                return;
-            }
-
-            var $ = cheerio.load(body);
-
-            growl.normal.notify('grab-packt', 'Book Title: ' + bookTitle);
-            growl.normal.notify('grab-packt', 'Claim URL: https://www.packtpub.com' + getBookUrl);
-            console.log('Book Title: ' + bookTitle);
-            console.log('Claim URL: https://www.packtpub.com' + getBookUrl);
-            console.log('----------- Packt Grab Done --------------');
-        });
-    });
+    growl.normal.notify('grab-packt: Book of the Day',bookTitle);
+    console.debug('grab-packt: Book of the Day: ' + bookTitle);
+    console.log('----------- Packt Grab Done --------------');
 });
